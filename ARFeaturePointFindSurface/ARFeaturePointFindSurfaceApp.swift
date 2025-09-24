64f9a939101f5dbc75ca51d8c11eb36762460c99
sgkim <sg.kim@curvsurf.com>
2025-09-24 17:05:04 +0900

    Initial Commit

---
+++ ARFeaturePointFindSurface/ARFeaturePointFindSurfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARFeaturePointFindSurfaceApp.swift
+//  ARFeaturePointFindSurface
+//
+//  Created by CurvSurf-SGKim on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ARFeaturePointFindSurfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARFeaturePointFindSurface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARFeaturePointFindSurface
+//
+//  Created by CurvSurf-SGKim on 9/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
